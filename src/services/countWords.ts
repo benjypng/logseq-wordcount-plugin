409c4d4d54c37d3da694c0273c62699f5bb71069
hkgnp <github.com/hkgnp>
2023-04-28 13:49:48 +0800

    [fix] error message when switching between pages

--- src/services/countWords.ts
+++ src/services/countWords.ts
@@ -1,43 +1,34 @@
 // Credit to https://stackoverflow.com/users/11854986/ken-lee for the below function
 export function mixedWordsFunction(str: string): number {
-  try {
-    /// fix problem in special characters such as middle-dot, etc.
-    str = str.replace(/[\u007F-\u00FE.,\/#!$%\^&*;:{}=\_`~()>\\]/g, " ");
+  if (str.length === 0) return 0;
 
-    /// make a duplicate first...
-    let str1 = str;
-    let str2 = str;
+  /// fix problem in special characters such as middle-dot, etc.
+  str = str.replace(/[\u007F-\u00FE.,\/#!$%\^&*;:{}=\_`~()>\\]/g, " ");
 
-    /// the following remove all chinese characters and then count the number of english characters in the string
-    str1 = str1.replace(/[^!-~\d\s]+/gi, " ");
+  /// make a duplicate first...
+  let str1 = str;
+  let str2 = str;
 
-    /// the following remove all english characters and then count the number of chinese characters in the string
-    str2 = str2.replace(/[!-~\d\s]+/gi, "");
+  /// the following remove all chinese characters and then count the number of english characters in the string
+  str1 = str1.replace(/[^!-~\d\s]+/gi, " ");
 
-    const matches1 = str1
-      .match(/[\u00ff-\uffff]|\S+/g)
-      ?.filter((i) => i !== "-");
-    const matches2 = str2.match(/[\u00ff-\uffff]|\S+/g);
+  /// the following remove all english characters and then count the number of chinese characters in the string
+  str2 = str2.replace(/[!-~\d\s]+/gi, "");
 
-    const count1 = matches1 ? matches1.length : 0;
-    const count2 = matches2 ? matches2.length : 0;
+  const matches1 = str1.match(/[\u00ff-\uffff]|\S+/g)?.filter((i) => i !== "-");
+  const matches2 = str2.match(/[\u00ff-\uffff]|\S+/g);
 
-    /// return the total of the mixture
-    return count1 + count2;
-  } catch (e) {
-    console.log(e);
-    throw Error("There needs to be a string.");
-  }
+  const count1 = matches1 ? matches1.length : 0;
+  const count2 = matches2 ? matches2.length : 0;
+
+  /// return the total of the mixture
+  return count1 + count2;
 }
 
 // To force counting of Cyrillic characters as words, e.g. for Russian
 export function simpleWordsFunction(str: string) {
-  try {
-    return str.split(" ").filter(function (n) {
-      return n != "";
-    }).length;
-  } catch (e) {
-    console.log(e);
-    throw Error("There needs to be a string.");
-  }
+  if (str.length === 0) return 0;
+  return str.split(" ").filter(function (n) {
+    return n != "";
+  }).length;
 }

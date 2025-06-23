a6bee346c80143e70f1d25a8c4772cf59be3cef5
Marc Butler <135883+marcbutler@users.noreply.github.com>
2025-06-23 22:52:12 +1000

    Set target percentage precision to one digit

--- src/services/create-wordcharcount-text.ts
+++ src/services/create-wordcharcount-text.ts
@@ -22,7 +22,7 @@ export const createWordcountText = (
   } else {
     // Session text
     const sessionTarget = parseInt(target)
-    const percentage = (totalCount / sessionTarget) * 100
+    const percentage = ((totalCount / sessionTarget) * 100).toFixed(1)
 
     switch (true) {
       case type.startsWith(':wordcount_'):
